c61d00ed01721a3f5d4d5aaf5611a14c6a2974b8
Sasha Tikhonov <atihonov1606@gmail.com>
2022-10-25 23:04:24 +0300

    шаг 3. реализовал работу инпутов попапа

--- pages/script.js
+++ pages/script.js
@@ -1,16 +1,37 @@
+// Темплейты
 const content = document.querySelector('.content');
-const popup__opened = content.querySelector('.popup__opened');
+
+// Врапперы
+const modalWindow = content.querySelector('.popup');
+const form = modalWindow.querySelector('.popup__container');
+
+//Кнопки и прочие дом узлы
 const editButton = content.querySelector('.profile__editButton');
-const form = content.querySelector('div.popup .popup__container');
 const closeButton = form.querySelector('.popup__closeButton');
 
-function popupWork() {
-  popup__opened.classList.toggle('popup__opened')
+//DOM узлы профиля
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__subtitle');
+
+// Данные форм и элементы форм
+const titleInputValue = form.querySelector('.popup__name');
+const descriptionInputValue = form.querySelector('.popup__job');
+
+function openModalWindow() {
+  modalWindow.classList.remove('popup__opened');
+  titleInputValue.value = profileTitle.textContent;
+  descriptionInputValue.value = profileDescription.textContent;
+}
+
+function closeModalWindow() {
+  modalWindow.classList.add('popup__opened');
 }
 
 form.addEventListener('submit', (evt) => {
-  evt.preventDefault()
-})
+  evt.preventDefault();
+  profileTitle.textContent = titleInputValue.value;
+  profileDescription.textContent = descriptionInputValue.value;
+  modalWindow.classList.add('popup__opened');
+  })
 
-editButton.addEventListener('click', popupWork);
-closeButton.addEventListener('click', popupWork);
+editButton.addEventListener('click', openModalWindow);
