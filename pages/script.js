e9491cc1352432296ae9077e51c3cba65aa78d27
Sasha Tikhonov <atihonov1606@gmail.com>
2022-10-25 22:10:17 +0300

    шаг 2. реализовал закрытие попапа и убрал автообновление страницы

--- pages/script.js
+++ pages/script.js
@@ -1,11 +1,16 @@
 const content = document.querySelector('.content');
 const popup__opened = content.querySelector('.popup__opened');
 const editButton = content.querySelector('.profile__editButton');
-const closeButton = content.querySelector('div.popup form.popup__container .popup__closeButton');
+const form = content.querySelector('div.popup .popup__container');
+const closeButton = form.querySelector('.popup__closeButton');
 
 function popupWork() {
   popup__opened.classList.toggle('popup__opened')
 }
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault()
+})
 
 editButton.addEventListener('click', popupWork);
 closeButton.addEventListener('click', popupWork);
