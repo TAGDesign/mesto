ea7a673d74276364a69cb3342b1bdf1e82964232
Sasha Tikhonov <atihonov1606@gmail.com>
2022-11-14 22:30:09 +0300

    refactor: оптимизация верстки попапа и js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 const content = document.querySelector('.content');
 const modalWindow = document.querySelector('.popup');
 const form = modalWindow.querySelector('.popup__container');
-const editButton = content.querySelector('.profile__editButton');
 const closeButton = form.querySelector('.popup__closeButton');
+const editButton = content.querySelector('.profile__editButton');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__subtitle');
 const titleInputValue = form.querySelector('.popup__name');
@@ -12,10 +12,6 @@ function openModalWindow() {
   modalWindow.classList.remove('popup__opened');
   titleInputValue.value = profileTitle.textContent;
   descriptionInputValue.value = profileDescription.textContent;
-}
-
-function closeModalWindow() {
-  modalWindow.classList.add('popup__opened');
 }
 
 form.addEventListener('submit', (evt) => {
