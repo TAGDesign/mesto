f8202da26a5255fe9240a35b7e9c33705aea9bb7
Sasha Tikhonov <atihonov1606@gmail.com>
2022-12-08 23:28:26 +0300

    добавил карточки с помощью innerHtml

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,51 @@
-const content = document.querySelector('.content');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function createElement(item) {
+  const template = `
+    <article class="element">
+      <img class="element__image" src="${item.link}" alt="руины каменного храма на фоте леса">
+      <div class="element__line">
+        <h2 class="element__title">${item.name}</h2>
+        <button class="element__favorite-button" aria-label="Избранное" type="button"></button>
+      </div>
+    </article>
+  `
+  console.log(template);
+  initialListCard.innerHTML += template;
+};
+
+initialCards.forEach(function(item) {
+  createElement(item);
+});
+
+
+
+const initialListCard = document.querySelector('.elements');
 const modalWindow = document.querySelector('.popup');
 const form = document.querySelector('.popup__form');
 const popupCloseButton = document.querySelector('.popup__close-button');
